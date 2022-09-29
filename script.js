b81ed5d3cdd6867ccb6c5482916ab1c8290737db
pokerhawk <eliabedosreis@gmail.com>
2022-09-29 12:09:09 -0300

    fix: all functions working except for 1

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let contador = 0;
+
 const lista_de_participantes = [
   {
     login: "eliabe",
-    senha: "hubs",
+    senha: "123",
     divida: 20.50,
   },
   {
@@ -39,10 +41,10 @@ function atualizarProdutos(){
   divProdutos.appendChild(ul)
   for(i in lista_de_produtos){
     const produtos = document.getElementById("produtos");
-    const newElement = document.createElement("li");
+    const li = document.createElement("li");
     const text = document.createTextNode(`${lista_de_produtos[i].quantidade} ${lista_de_produtos[i].produto} - ${lista_de_produtos[i].preco}R$ / Codigo do Produto: ${lista_de_produtos[i].codigo}`);
-    newElement.appendChild(text);
-    produtos.appendChild(newElement)
+    li.appendChild(text);
+    produtos.appendChild(li)
   }
 }
 atualizarProdutos();
@@ -53,26 +55,46 @@ function removerProdutos(){
   divProdutos.removeChild(produtos)
 }
 
-function adicionarCarrinho() {
-  const codigo_produto = document.getElementById("codigo_produto").value;
-  const quantidade_produto = document.getElementById("quantidade_produto").value;
+function removerCarrinho(){
+  const divCarrinho = document.getElementById("divCarrinho");
   const carrinho = document.getElementById("carrinho");
-  const newElement = document.createElement("li");
+  divCarrinho.removeChild(carrinho);
+}
+
+function atualizarCarrinho() {
+  if(!document.getElementById("carrinho")){
+    const divCarrinho = document.getElementById("divCarrinho");
+    const ul = document.createElement("ul");
+    ul.setAttribute('id','carrinho');
+    divCarrinho.appendChild(ul);
+  }
+  const codigo_produto = document.getElementById("codigo_produto").value;
+  const quantidade_produto = Number(document.getElementById("quantidade_produto").value);
+  const carrinho = document.getElementById("carrinho");
   for(i in lista_de_produtos){
     if(codigo_produto == lista_de_produtos[i].codigo){
       lista_de_produtos[i].quantidade = lista_de_produtos[i].quantidade - quantidade_produto
-      const text = document.createTextNode(`${quantidade_produto} ${lista_de_produtos[i].produto}`);
-      newElement.appendChild(text);
-      carrinho.appendChild(newElement)
+      contador += quantidade_produto;
+      const text = document.createTextNode(`${contador} ${lista_de_produtos[i].produto}`);
+      const textVerifica = `${contador-quantidade_produto} ${lista_de_produtos[i].produto}`;
+      const pnc = document.getElementsByClassName("produtos_no_carrinho");
+      for(i in pnc){ // FIX THIS SHEEEET
+        if(pnc[i].innerText == textVerifica){
+          pnc[i].innerText = `${contador} ${lista_de_produtos[i].produto}`;
+          break;
+        } else{
+          const li = document.createElement("li");
+          li.setAttribute('class','produtos_no_carrinho')
+          li.appendChild(text);
+          carrinho.appendChild(li)
+          break;
+        } // WE NEED TO RESET COUNTER: DO AN IF CONDITION TO RESET IT WHEN THEN CODE OF THE PRODUCT CHANGES LIKE A ONCHANGE
+      }
       break;
     }
   }
   removerProdutos()
   atualizarProdutos()
-}
-
-function atualizarCarrinho(){
-  //arrumar função acima e atualizar carrinho
 }
 
 const conferirDivida = () => {
@@ -98,3 +120,4 @@ document.getElementById("senha_input").addEventListener("keydown", function(even
 
 //PARTE DE TESTES
 //
+
