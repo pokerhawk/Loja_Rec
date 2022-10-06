b41990520d667549dea6e6ee8a3afc18db523d9e
pokerhawk <eliabedosreis@gmail.com>
2022-10-06 14:00:51 -0300

    feat: get/post methods for cadastro and conferirDivida

--- DB/server.js
+++ DB/server.js
@@ -1,44 +1,76 @@
-const express = require('express') // remove cors and pg-promise
-var cors = require('cors')
-const app = express()
+const express = require("express"); // remove cors and pg-promise
+var cors = require("cors");
+const app = express();
 const port = 3000;
-const knex = require('knex')({
-  client: 'mysql',
+const knex = require("knex")({
+  client: "mysql",
   connection: {
-    host : '127.0.0.1',
-    port : 3306,
-    user : 'root',
-    password : 'hubs',
-    database : 'loja'
-  }
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "hubs",
+    database: "loja",
+  },
 });
 
 const display = [];
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 ////////////////////////////////////// SERVER
-app.get('/', (req, res)=>{
-  knex.select('*').from('users').then(data=>{
-    display.push(data)
-    res.json(data)
-  }).catch(error=>{console.log(error)})
-})
-
-app.post('/', (req, res)=>{
-  knex.transaction(trx=>{
-    trx.insert({
-      nome: 'joao',
-      senha: '321',
-      divida: 5
+app.get("/", (req, res) => {
+  knex
+    .select("*")
+    .from("users")
+    .then((data) => {
+      display.push(data);
+      res.json(data);
     })
-  })
-  .then(res=>{
-    console.log(res)
-  })
-  .catch(err=>{console.log(err)})
-})
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.post("/post", async (req, res) => {
+  const { nome, senha } = req.body;
+  if (!nome || !senha) {
+    return res.status(400).json("Dados incorretos!");
+  }
+  knex
+    .transaction((trx) => {
+      trx
+        .insert({
+          nome: nome,
+          senha: senha,
+          divida: 0,
+        })
+        .into("users")
+        .then(res.json("Cadastrado com sucesso!"))
+        .then(trx.commit)
+        .catch(trx.rollback);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/cadastro", (req, res) => {
+  knex
+    .transaction((trx) => {
+      trx.insert({
+        nome: "joao",
+        senha: "321",
+        divida: 5,
+      });
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // app.delete('/', (req, res)=>{ // CHECK
 //   knex('users').dropColumn().where('nome', 'joao');
@@ -47,30 +79,37 @@ app.post('/', (req, res)=>{
 //   .catch(err=>{console.log(err)})
 // })
 
-
-
 ////////////////////////////////////// CONSOLE LOG DISPLAYS
-knex.select('*').from('users').then(data=>{
-  console.log(data)
-}).catch(error=>{console.log(error)})
+knex
+  .select("*")
+  .from("users")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-knex.select('*').from('users').where({
-  nome: 'eliabe'
-})
-.then(data=>{
-  console.log(data)
-})
+knex
+  .select("*")
+  .from("users")
+  .where({
+    nome: "eliabe",
+  })
+  .then((data) => {
+    console.log(data);
+  });
 
 // knex.schema.alterTable('users',table=>{
 //   table.add;
 // })
-
 
 //////////////////////////////////////
 app.get("/", (req, res) => {
   res.json(display); //WHAT WILL BE DISPLAYED ON BROWSER
 });
 
-app.listen(port, ()=>{
-  console.log(`Server running at https:localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server running at https:localhost:${port}`);
+});
+
--- script.js
+++ script.js
@@ -1,137 +1,163 @@
-const cadastro_nome = document.getElementById('cadastro_nome'); // make function
-const cadastro_senha = document.getElementById('cadastro_senha');
+const cadastro_nome = document.getElementById("cadastro_nome"); // make function
+const cadastro_senha = document.getElementById("cadastro_senha");
 let contador = 0; // CHANGE/REMOVE THIS
 
-const lista_de_participantes = [ //PLACEHOLDER
+const lista_de_participantes = [
+  //PLACEHOLDER
   {
     login: "eliabe",
     senha: "123",
-    divida: 20.50,
+    divida: 20.5,
   },
   {
     login: "lizuly",
     senha: "123",
-    divida: 10.00,
+    divida: 10.0,
   },
 ];
 
-const lista_de_produtos = [ //PLACEHOLDER
+const lista_de_produtos = [
+  //PLACEHOLDER
   {
     codigo: "0001",
     produto: "caixinha",
-    preco: 13.20,
-    quantidade: 13
+    preco: 13.2,
+    quantidade: 13,
   },
   {
     codigo: "0002",
     produto: "livro",
-    preco: 10.90,
-    quantidade: 4
+    preco: 10.9,
+    quantidade: 4,
   },
   {
     codigo: "0003",
     produto: "caderno",
-    preco: 15.90,
-    quantidade: 7
-  }
+    preco: 15.9,
+    quantidade: 7,
+  },
 ];
 
-const  atualizarProdutos = () =>{
+const atualizarProdutos = () => {
   const divProdutos = document.getElementById("divProdutos");
   const ul = document.createElement("ul");
-  ul.setAttribute('id','produtos')
-  divProdutos.appendChild(ul)
-  for(i in lista_de_produtos){
+  ul.setAttribute("id", "produtos");
+  divProdutos.appendChild(ul);
+  for (i in lista_de_produtos) {
     const produtos = document.getElementById("produtos");
     const li = document.createElement("li");
-    const text = document.createTextNode(`${lista_de_produtos[i].quantidade} ${lista_de_produtos[i].produto} - ${lista_de_produtos[i].preco}R$ / Codigo do Produto: ${lista_de_produtos[i].codigo}`);
+    const text = document.createTextNode(
+      `${lista_de_produtos[i].quantidade} ${lista_de_produtos[i].produto} - ${lista_de_produtos[i].preco}R$ / Codigo do Produto: ${lista_de_produtos[i].codigo}`
+    );
     li.appendChild(text);
-    produtos.appendChild(li)
+    produtos.appendChild(li);
   }
-}
+};
 atualizarProdutos();
 
-const removerProdutos = () =>{
+const removerProdutos = () => {
   const divProdutos = document.getElementById("divProdutos");
   const produtos = document.getElementById("produtos");
-  divProdutos.removeChild(produtos)
-}
+  divProdutos.removeChild(produtos);
+};
 
-const removerCarrinho = () =>{ // NOT BEING USED
+const removerCarrinho = () => {
+  // NOT BEING USED
   const divCarrinho = document.getElementById("divCarrinho");
   const carrinho = document.getElementById("carrinho");
   divCarrinho.removeChild(carrinho);
-}
+};
 
-const atualizarCarrinho = () => { // CHANGE COUNTER, FIX VARIABLES, FIND BETTER WAY TO DO ALL THIS
-  if(!document.getElementById("carrinho")){
+const atualizarCarrinho = () => {
+  // CHANGE COUNTER, FIX VARIABLES, FIND BETTER WAY TO DO ALL THIS
+  if (!document.getElementById("carrinho")) {
     const divCarrinho = document.getElementById("divCarrinho");
     const ul = document.createElement("ul");
-    ul.setAttribute('id','carrinho');
+    ul.setAttribute("id", "carrinho");
     divCarrinho.appendChild(ul);
   }
   const codigo_produto = document.getElementById("codigo_produto").value;
-  const quantidade_produto = Number(document.getElementById("quantidade_produto").value);
+  const quantidade_produto = Number(
+    document.getElementById("quantidade_produto").value
+  );
   const carrinho = document.getElementById("carrinho");
-  for(i in lista_de_produtos){
-    if(codigo_produto == lista_de_produtos[i].codigo){
-      lista_de_produtos[i].quantidade = lista_de_produtos[i].quantidade - quantidade_produto
+  for (i in lista_de_produtos) {
+    if (codigo_produto == lista_de_produtos[i].codigo) {
+      lista_de_produtos[i].quantidade =
+        lista_de_produtos[i].quantidade - quantidade_produto;
       contador += quantidade_produto;
-      const text = document.createTextNode(`${contador} ${lista_de_produtos[i].produto}`);
-      const textVerifica = `${contador-quantidade_produto} ${lista_de_produtos[i].produto}`;
+      const text = document.createTextNode(
+        `${contador} ${lista_de_produtos[i].produto}`
+      );
+      const textVerifica = `${contador - quantidade_produto} ${
+        lista_de_produtos[i].produto
+      }`;
       const pnc = document.getElementsByClassName("produtos_no_carrinho");
-      for(i in pnc){ // FIX NEEDED
-        if(pnc[i].innerText == textVerifica){
+      for (i in pnc) {
+        // FIX NEEDED
+        if (pnc[i].innerText == textVerifica) {
           pnc[i].innerText = `${contador} ${lista_de_produtos[i].produto}`;
           break;
-        } else{
+        } else {
           const li = document.createElement("li");
-          li.setAttribute('class','produtos_no_carrinho')
+          li.setAttribute("class", "produtos_no_carrinho");
           li.appendChild(text);
-          carrinho.appendChild(li)
+          carrinho.appendChild(li);
           break;
         } // WE NEED TO RESET COUNTER: DO AN IF CONDITION TO RESET IT WHEN THE CODE OF THE PRODUCT CHANGES LIKE A ONCHANGE
       }
       break;
     }
   }
-  removerProdutos()
-  atualizarProdutos()
-}
+  removerProdutos();
+  atualizarProdutos();
+};
 
 const conferirDivida = async () => {
-  const response = await fetch('http:/localhost:3000/')
+  const response = await fetch("http:/localhost:3000/");
   const result = await response.json();
   const login = document.getElementById("login_input").value;
   const senha = document.getElementById("senha_input").value;
   let divida = document.getElementById("resposta_dividendo");
-  for(i in result){
-    if(login == result[i].nome && senha == result[i].senha){
+  for (i in result) {
+    if (login == result[i].nome && senha == result[i].senha) {
       divida.innerText = `${result[i].nome} você está devendo R$${result[i].divida}`;
       break;
     }
   }
 };
 
-const cadastrar = async () =>{
-  const response = await fetch('http:/localhost:3000/',{
-    method: 'post'
-  })
+const cadastrar = async () => {
+  const cadastro_nome = document.getElementById("cadastro_nome").value;
+  const cadastro_senha = document.getElementById("cadastro_senha").value;
+  const response = await fetch("http:/localhost:3000/post", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      nome: cadastro_nome,
+      senha: cadastro_senha,
+      divida: 0,
+    }),
+  });
   const result = await response.json();
-  console.log(result[0])
-}
+  console.log(result);
+};
 
-document.getElementById("senha_input").addEventListener("keydown", function(event){
-  if(event.key == "Enter"){
-    conferirDivida()
-  }
-})
+document
+  .getElementById("senha_input")
+  .addEventListener("keydown", function (event) {
+    if (event.key == "Enter") {
+      conferirDivida();
+    }
+  });
 
 const getData = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/')
+  //TESTE: DELETAR DEPOIS
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
   const result = await response.json();
-  console.log(result)
-}
+  console.log(result);
+};
 
 //PARTE DE TESTES
 //
+
