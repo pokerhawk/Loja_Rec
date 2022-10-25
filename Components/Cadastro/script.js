7ce2f5c624c25392b0a2a2080bb21453ce4e505d
pokerhawk <eliabedosreis@gmail.com>
2022-10-25 16:53:52 -0300

    test: testing input>image>server>DB

--- Components/Cadastro/script.js
+++ Components/Cadastro/script.js
@@ -1,22 +1,26 @@
 const cadastrar = async () => {
-    const cadastro_nome = document.getElementById("cadastro_nome").value;
-    const cadastro_senha = document.getElementById("cadastro_senha").value;
-    const response = await fetch("http:/localhost:3000/cadastro", {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        nome: cadastro_nome,
-        senha: cadastro_senha,
-      }),
-    });
-    const result = await response.json();
-    document.getElementById("cadastro_nome").value = "";
-    document.getElementById("cadastro_senha").value = "";
-    alert(result);
-  };
+  const cadastro_nome = document.getElementById("cadastro_nome").value;
+  const cadastro_senha = document.getElementById("cadastro_senha").value;
+  const imagem = document.getElementById("imageFile").files[0];
+  let formData = new FormData();
+  formData.append("fileToUpload", imagem);
+  const response = await fetch("http:/localhost:3000/cadastro", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      nome: cadastro_nome,
+      senha: cadastro_senha,
+      // imagem: formData // OLHAR ISSO
+    }),
+  });
+  const result = await response.json();
+  document.getElementById("cadastro_nome").value = "";
+  document.getElementById("cadastro_senha").value = "";
+  alert(result);
+};
 
 document.getElementById("cadastro_senha").addEventListener("keydown",(event)=>{
-    if(event.key=="Enter"){
-      cadastrar();
-    }
-  })
+  if(event.key=="Enter"){
+    cadastrar();
+  }
+})
